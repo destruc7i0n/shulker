889f9616c4645b058772a31a1b6eb6c85ca55a46
secret_online <me+github@secretonline.co>
2016-01-18 20:55:46 +1300

    Beautify with JS Beautify
    Embrace the whitespace. Readability is a great thing.

--- index.js
+++ index.js
@@ -4,8 +4,8 @@
 var Discord = require("discord.js");
 var Rcon = require("rcon");
 var express = require("express");
-var app = express(); 
-var http = require("http").Server(app); 
+var app = express();
+var http = require("http").Server(app);
 var c = require("./config.json");
 var debug = c.DEBUG;
 var shulker = new Discord.Client();
@@ -13,9 +13,9 @@ var shulker = new Discord.Client();
 var client = new Rcon(c.MINECRAFT_SERVER_RCON_IP, c.MINECRAFT_SERVER_RCON_PORT, c.MINECRAFT_SERVER_RCON_PASSWORD);
 
 client.on("auth", function() {
-    console.log("[INFO] Authenticated with "+c.MINECRAFT_SERVER_RCON_IP+":"+c.MINECRAFT_SERVER_RCON_PORT);
+    console.log("[INFO] Authenticated with " + c.MINECRAFT_SERVER_RCON_IP + ":" + c.MINECRAFT_SERVER_RCON_PORT);
 }).on("response", function(str) {
-    if(debug && str) {
+    if (debug && str) {
         console.log("[DEBUG] Got response: " + str);
     }
 }).on("end", function() {
@@ -25,42 +25,44 @@ client.on("auth", function() {
 client.connect();
 
 app.use(function(request, response, next) {
-  request.rawBody = "";
-  request.setEncoding("utf8");
+    request.rawBody = "";
+    request.setEncoding("utf8");
 
-  request.on("data", function(chunk) { 
-      request.rawBody += chunk;
-  });
+    request.on("data", function(chunk) {
+        request.rawBody += chunk;
+    });
 
-  request.on("end", function() {
-      next();
-  });
+    request.on("end", function() {
+        next();
+    });
 });
 
 shulker.on("ready", function() {
-    var channel = shulker.channels.get("name", c.DISCORD_CHANNEL).id; 
-    app.post(c.WEBHOOK, function(request, response){
+    var channel = shulker.channels.get("name", c.DISCORD_CHANNEL).id;
+    app.post(c.WEBHOOK, function(request, response) {
         var body = request.rawBody;
-        console.log("[INFO] Recieved "+body);
+        console.log("[INFO] Recieved " + body);
         var re = new RegExp(c.REGEX_MATCH_CHAT_MC);
         var ignored = new RegExp(c.REGEX_IGNORED_CHAT);
-        if(!ignored.test(body)) {
+        if (!ignored.test(body)) {
             var bodymatch = body.match(re);
-            if(debug) {
-                console.log("[DEBUG] Username: "+bodymatch[1]);
-                console.log("[DEBUG] Text: "+bodymatch[2]);
+            if (debug) {
+                console.log("[DEBUG] Username: " + bodymatch[1]);
+                console.log("[DEBUG] Text: " + bodymatch[2]);
             }
-            var message = "**"+bodymatch[1]+"**: "+bodymatch[2];
+            var message = "**" + bodymatch[1] + "**: " + bodymatch[2];
             shulker.channels.get("id", channel).sendMessage(message);
         }
         response.send("");
     });
 });
 
-shulker.on("message", function (message) {
-    if(message.author.id !== shulker.user.id) {
-        var data = { text: "<"+message.author.username+"> "+message.content };
-        client.send('tellraw @a ["",'+JSON.stringify(data)+']');
+shulker.on("message", function(message) {
+    if (message.author.id !== shulker.user.id) {
+        var data = {
+            text: "<" + message.author.username + "> " + message.content
+        };
+        client.send('tellraw @a ["",' + JSON.stringify(data) + ']');
     }
 });
 
@@ -69,11 +71,11 @@ shulker.login(c.DISCORD_EMAIL, c.DISCORD_PASSWORD);
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || process.env.IP || "127.0.0.1";
 var serverport = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || c.PORT;
 if (process.env.OPENSHIFT_NODEJS_IP !== undefined) {
-    http.listen( serverport, ipaddress, function() {
+    http.listen(serverport, ipaddress, function() {
         console.log("[INFO] Bot listening on *:" + serverport);
     });
 } else {
-    http.listen( serverport, function() {
+    http.listen(serverport, function() {
         console.log("[INFO] Bot listening on *:" + c.PORT);
     });
 }
