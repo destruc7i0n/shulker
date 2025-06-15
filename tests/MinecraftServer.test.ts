8160c2e8125f535401eefaee2b77ec81a0754350
destruc7i0n <destruc7i0n@users.noreply.github.com>
2025-06-15 00:16:08 -0700

    clean slate

--- tests/MinecraftServer.test.ts
+++ tests/MinecraftServer.test.ts
@@ -30,6 +30,17 @@ const serverProperties = {
   'rcon.port': RCON_PORT.toString(),
 }
 
+const downloadServer = () => {
+  return new Promise((resolve, reject) => {
+    download(MC_VERSION, MC_SERVER_JAR, (err: any) => {
+      if (err) {
+        reject(err)
+      }
+      resolve(void 0)
+    })
+  })
+}
+
 describe(`MinecraftServer v${MC_VERSION}`, () => {
   jest.setTimeout(1000 * 60) // 1 minute
   
@@ -37,43 +48,34 @@ describe(`MinecraftServer v${MC_VERSION}`, () => {
   let wrap: typeof Wrap
   let rcon: Rcon
 
-  beforeAll((done) => {
-    console.log(`Downloading Minecraft ${MC_VERSION} server...`)
-    
-    download(MC_VERSION, MC_SERVER_JAR, (err: any) => {
-      if (err) {
-        console.error(err)
-        done(err)
-        return
-      }
-      console.log(`Minecraft ${MC_VERSION} server JAR downloaded`)
-      done()
-    })
-  })
-
   beforeEach((done) => {
     // Clear previous logs
     serverLog.mockClear()
 
-    wrap = new Wrap(MC_SERVER_JAR, MC_SERVER_PATH)
-    
-    wrap.on('line', (line: string) => {
-      console.log(`[${MC_VERSION} SERVER] ${line}`)
-      serverLog(line)
-    })
+    downloadServer().then(() => {
+      wrap = new Wrap(MC_SERVER_JAR, MC_SERVER_PATH)
+      
+      wrap.on('line', (line: string) => {
+        console.log(`[${MC_VERSION} SERVER] ${line}`)
+        serverLog(line)
+      })
 
-    console.log(`Starting fresh Minecraft ${MC_VERSION} server instance...`)
-    wrap.startServer(serverProperties, (err: any) => {
-      if (err) {
-        console.error(err)
-        done(err)
-        return
-      }
+      console.log(`Starting fresh Minecraft ${MC_VERSION} server instance...`)
+      wrap.startServer(serverProperties, (err: any) => {
+        if (err) {
+          console.error(err)
+          done(err)
+          return
+        }
 
-      rcon = new Rcon(configWithServer.MINECRAFT_SERVER_RCON_IP, configWithServer.MINECRAFT_SERVER_RCON_PORT, configWithServer.DEBUG)
-      rcon.auth(configWithServer.MINECRAFT_SERVER_RCON_PASSWORD).then(() => {
-        console.log(`[${MC_VERSION} RCON] Connected and authenticated`)
-        done()
+        rcon = new Rcon(configWithServer.MINECRAFT_SERVER_RCON_IP, configWithServer.MINECRAFT_SERVER_RCON_PORT, configWithServer.DEBUG)
+        rcon.auth(configWithServer.MINECRAFT_SERVER_RCON_PASSWORD).then(() => {
+          console.log(`[${MC_VERSION} RCON] Connected and authenticated`)
+          done()
+        }).catch((err: any) => {
+          console.error(err)
+          done(err)
+        })
       })
     })
   })
@@ -84,21 +86,14 @@ describe(`MinecraftServer v${MC_VERSION}`, () => {
       if (err) {
         console.error(err)
       }
-      done()
+      wrap.deleteServerData((err: any) => {
+        if (err) {
+          console.log(err)
+        }
+        done(err)
+      })
     })
   })
-
-  afterAll((done) => {
-    console.log(`Cleaning up server files...`)
-    wrap.deleteServerData((err: any) => {
-      if (err) {
-        console.log(err)
-      }
-      done(err)
-    })
-  })
-
-  // with 1.19.2 the name is surrounded with brackets rather than <>
 
   it('reads logs from Minecraft server', (done) => {
     const handler = new MinecraftHandler(configWithServer)
